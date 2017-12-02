b31adb83cacb5de0a48401ffa6d365fe0ce4a5eb
BluCode <pub@jackc.info>
2017-12-02 16:19:36 -0500

    Improve keyboard driver

--- drivers/keyboard.c
+++ drivers/keyboard.c
@@ -12,12 +12,7 @@
 static char key_buffer[256];
 
 #define SC_MAX 57
-const char *sc_name[] = { "ERROR", "Esc", "1", "2", "3", "4", "5", "6", 
-    "7", "8", "9", "0", "-", "=", "Backspace", "Tab", "Q", "W", "E", 
-        "R", "T", "Y", "U", "I", "O", "P", "[", "]", "Enter", "Lctrl", 
-        "A", "S", "D", "F", "G", "H", "J", "K", "L", ";", "'", "`", 
-        "LShift", "\\", "Z", "X", "C", "V", "B", "N", "M", ",", ".", 
-        "/", "RShift", "Keypad *", "LAlt", "Spacebar"};
+#define REAL_SC_MAX 80
 const char sc_ascii[] = { '?', '?', '1', '2', '3', '4', '5', '6',     
     '7', '8', '9', '0', '-', '=', '?', '?', 'Q', 'W', 'E', 'R', 'T', 'Y', 
         'U', 'I', 'O', 'P', '[', ']', '?', '?', 'A', 'S', 'D', 'F', 'G', 
@@ -28,21 +23,16 @@ static void keyboard_callback(registers_t regs) {
     /* The PIC leaves us the scancode in port 0x60 */
     u8 scancode = port_byte_in(0x60);
     
-    if (scancode > SC_MAX) {
-        char scancode_ascii[256];
-        int_to_ascii((int)scancode, scancode_ascii);
-        kprint("Got unknown scancode: ");
-        kprint(scancode_ascii);
+    if (scancode > REAL_SC_MAX) {
         return;
-    };
+    }
     if (scancode == BACKSPACE) {
         backspace(key_buffer);
         kprint_backspace();
-    } else if (scancode == ENTER) {
-        kprint("\n");
-        user_input(key_buffer); /* kernel-controlled function */
+    } else if (scancode > SC_MAX || sc_ascii[(int)scancode] == '?') {
+        user_input(key_buffer, (int)scancode); /* kernel-controlled function */
         key_buffer[0] = '\0';
-    } else {
+    } else { /* Text scancode max */
         char letter = sc_ascii[(int)scancode];
         /* Remember that kprint only accepts char[] */
         char str[2] = {letter, '\0'};
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -11,12 +11,16 @@ void main() {
         "Type END to halt the CPU\n> ");
 }
 
-void user_input(char *input) {
+void user_input(char *input, int scancode) {
     if (strcmp(input, "END") == 0) {
         kprint("Stopping the CPU. Bye!\n");
         asm volatile("hlt");
     }
     kprint("You said: ");
     kprint(input);
+    kprint(" with scancode: ");
+    char sc[32];
+    int_to_ascii(scancode, sc);
+    kprint(sc);
     kprint("\n> ");
 }
